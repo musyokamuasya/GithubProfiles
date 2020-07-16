38398eec6e7566425a78ea5fb30b166c31c37e3a
Musyoka Muasya <josephmuasya8@gmail.com>
2020-07-16 22:54:09 +0300

    Fetching user profile data from github api

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // The application's main entry point
+
+const github = new Github();
+const searchUser = document.querySelector('#search-user');
+searchUser.addEventListener('keyup', (e) => {
+    // Get the text entered and search from github api
+    const userText = e.target.value;
+    if (userText !== '') {
+        github.getUser(userText).then(data => {
+            if (data.profile.message === 'Not Found') {
+                //     Show error message & Persist the current user
+            } else {
+                //  Show the profile   
+            }
+        });
+    } else {
+        //     Clear the profile data
+    }
+});
--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 // Contains the server API from github
+
+class Github {
+    constructor() {
+            this.client_id = '23a924576c4a2594772d';
+            this.client_secret = '4a616b0ac38c3aa03dfcd09852d4c4c51af5427f';
+        }
+        //     Get the user's profile
+    async getUser(user) {
+        const userProfile = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        // Get profile json data
+        const profile = await userProfile.json();
+        return { profile };
+    }
+}
